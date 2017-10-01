bd9a7f45b9380c6dbb0292036ff9277df0a07e44
Salman Tariq <salman@diligensoftware.com>
2017-09-30 21:40:19 -0400

    ingredient menu items

--- public/dist/ingredients.bundle.js
+++ public/dist/ingredients.bundle.js
@@ -71,6 +71,24 @@
 "use strict";
 
 
+var options = {
+	'My identity': ['White', 'Black or Latin', 'Asian', 'Other'],
+	'My gender': ['Male', 'Female', 'Trans', 'Other'],
+	'My parents': ['Both parents went to college', 'One parent went to college', 'No parent went to college', 'First to go to college'],
+	'My family': ['Broke', 'Middle Class', 'Upper Middle Class', 'Rich'],
+	'How I paid for school': ['No Loans', 'Loans', 'Scholarships, Grants', 'Pell Grants'],
+	'Why I chose my school': ['Specific Program', 'Rank / Reputation', 'Cost', 'Family'],
+	'My life on campus': ['Mostly lived on campus', 'Worked', 'Fraternities / Sororities', 'Varsity Sports'],
+	'My getting used to college': ['Remedial Course', 'Active Campus Life', 'Happy with grades'],
+	'Becoming a senior': ['Internships Junior Year', 'Declare Major', 'Job Search', 'No Plans For Jobs'],
+	'My major': ['SCIENCE, TECHNOLOGY, ENGINEERING OR MATH', 'ECONOMICS, FINANCE, OR ACCOUNTING', 'FINE ARTS, MUSIC, DRAMA, OR PERFORMING ARTS', 'ENVIRONMENTAL STUDIES, FORESTRY, OR ECOLOGY	LIBERAL ARTS, HISTORY, ANY LANGUAGE, OR PHILOSOPHY	PSYCHOLOGY, SOCIOLOGY, ANTHROPOLOGY, OR RELIGIOUS STUDIES	POLITICAL SCIENCE, PUBLIC POLICY, OR GOVERNMENT'],
+	'Effects of my student loans': ['WILL TAKE JOB OUTSIDE OF FIELD OF STUDY', 'WILL LESS DESIRABLE JOB', 'WILL WORK MORE THAN ONE JOB', 'WILL WORK MORE HOURS'],
+	'My certifications': ['STATE CERTIFICATIONS', 'PROFESSIONAL CERTIFICATIONS', 'JOB REQUIREMENT', 'PART OF COURSE'],
+	'Barriers beyond me': ['NO RIGHT CREDENTIALS / GRADES', 'CANT RELOCATE', 'LACK OF CONNECTIONS', 'NO RECRUTUING / CAREER SVCS HELP'],
+	'Serious life event': ['DIVORCE IN FAMILY', 'PARENTS JOB LOSS', 'ILLNESS', 'DEATH'],
+	'Other things': []
+};
+
 var ingredientsStart = 0;
 
 var url = window.location.href;
@@ -89,6 +107,7 @@ paramObj.transfer = parseInt(paramObj.transfer);
 paramObj.ingredients = paramObj.ingredients.split(',');
 
 var ingredientsContainer = document.getElementsByClassName('ingredients')[0];
+var ingredientMenuContainer = document.getElementsByClassName('ingredient-menu')[0];
 
 function drawIngredients(start, container) {
 	container.innerHTML = ''; // Clear
@@ -109,8 +128,29 @@ function drawIngredients(start, container) {
 		p.innerHTML = paramObj.ingredients[_i];
 		el.appendChild(p);
 
+		el.onclick = renderIngredientMenu(paramObj.ingredients[_i], ingredientMenuContainer);
+
 		container.appendChild(el);
 	}
+}
+
+function renderIngredientMenu(ingredient, container) {
+	return function (e) {
+		var menuItems = options[ingredient];
+		ingredientMenuContainer.innerHTML = ''; // Clear
+
+		for (var _i2 = 0; _i2 < menuItems.length; _i2++) {
+			var menuItem = document.createElement('div');
+			menuItem.classList.add('ingredient-menu--item');
+			menuItem.classList.add('padding-20');
+
+			var p = document.createElement('p');
+			p.innerHTML = menuItems[_i2];
+			menuItem.appendChild(p);
+
+			container.appendChild(menuItem);
+		}
+	};
 }
 
 document.getElementById('left').onclick = function () {
--- public/js/ingredients.js
+++ public/js/ingredients.js
@@ -1,3 +1,22 @@
+const options = {
+	'My identity': ['White', 'Black or Latin', 'Asian', 'Other'],
+	'My gender': ['Male', 'Female', 'Trans', 'Other'],
+	'My parents': ['Both parents went to college', 'One parent went to college', 'No parent went to college', 'First to go to college'],
+	'My family': ['Broke', 'Middle Class', 'Upper Middle Class', 'Rich'],
+	'How I paid for school': ['No Loans', 'Loans', 'Scholarships, Grants', 'Pell Grants'],
+	'Why I chose my school': ['Specific Program', 'Rank / Reputation', 'Cost', 'Family'],
+	'My life on campus': ['Mostly lived on campus', 'Worked', 'Fraternities / Sororities', 'Varsity Sports'],
+	'My getting used to college': ['Remedial Course', 'Active Campus Life',	'Happy with grades'],
+	'Becoming a senior': ['Internships Junior Year', 'Declare Major', 'Job Search', 'No Plans For Jobs'],
+	'My major':	['SCIENCE, TECHNOLOGY, ENGINEERING OR MATH', 'ECONOMICS, FINANCE, OR ACCOUNTING', 'FINE ARTS, MUSIC, DRAMA, OR PERFORMING ARTS', 'ENVIRONMENTAL STUDIES, FORESTRY, OR ECOLOGY	LIBERAL ARTS, HISTORY, ANY LANGUAGE, OR PHILOSOPHY	PSYCHOLOGY, SOCIOLOGY, ANTHROPOLOGY, OR RELIGIOUS STUDIES	POLITICAL SCIENCE, PUBLIC POLICY, OR GOVERNMENT'],
+	'Effects of my student loans': ['WILL TAKE JOB OUTSIDE OF FIELD OF STUDY', 'WILL LESS DESIRABLE JOB', 'WILL WORK MORE THAN ONE JOB', 'WILL WORK MORE HOURS'],
+	'My certifications': ['STATE CERTIFICATIONS', 'PROFESSIONAL CERTIFICATIONS', 'JOB REQUIREMENT', 'PART OF COURSE'],
+	'Barriers beyond me': ['NO RIGHT CREDENTIALS / GRADES', 'CANT RELOCATE', 'LACK OF CONNECTIONS', 'NO RECRUTUING / CAREER SVCS HELP'],	
+	'Serious life event': ['DIVORCE IN FAMILY', 'PARENTS JOB LOSS', 'ILLNESS', 'DEATH'],
+	'Other things': [],
+}
+
+
 let ingredientsStart = 0
 
 const url = window.location.href
@@ -14,6 +33,7 @@ paramObj.transfer = parseInt(paramObj.transfer)
 paramObj.ingredients = paramObj.ingredients.split(',')
 
 const ingredientsContainer = document.getElementsByClassName('ingredients')[0]
+const ingredientMenuContainer = document.getElementsByClassName('ingredient-menu')[0]
 
 function drawIngredients(start, container) {
 	container.innerHTML = '' // Clear
@@ -34,7 +54,28 @@ function drawIngredients(start, container) {
 		p.innerHTML = paramObj.ingredients[i]
 		el.appendChild(p)
 
+		el.onclick = renderIngredientMenu(paramObj.ingredients[i], ingredientMenuContainer)
+
 		container.appendChild(el)
+	}
+}
+
+function renderIngredientMenu(ingredient, container) {
+	return function(e) {
+		const menuItems = options[ingredient]
+		ingredientMenuContainer.innerHTML = '' // Clear
+
+		for (let i = 0; i < menuItems.length; i++) {
+			let menuItem = document.createElement('div')
+			menuItem.classList.add('ingredient-menu--item')
+			menuItem.classList.add('padding-20')
+
+			let p = document.createElement('p')
+			p.innerHTML = menuItems[i]
+			menuItem.appendChild(p)
+
+			container.appendChild(menuItem)
+		}
 	}
 }
 
