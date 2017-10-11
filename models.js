08003242b3e29407cf9a34fefd361bebe9f425b6
Salman Tariq <salman@diligensoftware.com>
2017-10-11 00:54:32 -0400

    modelling tables

--- models.js
+++ models.js
@@ -15,12 +15,43 @@ const knexConn = knex({
 });
 
 const DB = bookshelf(knexConn);
+DB.plugin('registry')
 
 
 const User = DB.Model.extend({
 	tableName: 'users'
 })
 
+const Role = DB.Model.extend({
+  tableName: 'roles',
+  jobs() {
+    return this.hasMany('Job')
+  },
+})
+
+const Applicant = DB.Model.extend({
+  tableName: 'applicants',
+  applicant() {
+    return this.hasOne('User')
+  },
+  job() {
+    return this.belongsTo('Job')
+  }
+})
+
+const Job = DB.Model.extend({
+  tableName: 'jobs',
+  applicants() {
+    return this.hasMany('Applicant')
+  },
+  role() {
+    return this.hasOne('Role')
+  }
+})
+
 module.exports = {
 	User,
+  Role,
+  Applicant,
+  Job,
 }
